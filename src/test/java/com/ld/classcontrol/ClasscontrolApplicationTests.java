4fe33c6cac32640f9dfa5d83fba37acf2fd80d23
DESKTOP-SOMFQTD\Luisdany <luisdany.pernillo@blautech.us>
2018-01-12 11:22:17 -0600

    Initial commit

---
+++ src/test/java/com/ld/classcontrol/ClasscontrolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ld.classcontrol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClasscontrolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
